70eda077186e1e8f7b23aa6733ee27a91fecc375
samcodee <samrridhn@gmail.com>
2021-04-24 12:17:04 +0530

    Shortened the code, added form validation and enhanced the UI

--- js/calculator.js
+++ js/calculator.js
@@ -15,6 +15,20 @@ function assignoperator(operator) {
     operate();
 }
 function operate() {
-    var operators = { '+': function (a, b, c, d, e) { return a + b + c + d + e }, '-': function (a, b, c, d, e) { return a - b - c - d - e }, '/': function (a, b, c, d, e) { return a / b / c / d / e }, '*': function (a, b, c, d, e) { return a * b * c * d * e}};
-    document.getElementById("final").innerHTML = operators[operatorglobal](one, two, third, fourth, fifth);
+    var operators = { '+': function (a, b, c, d, e) { return a + b + c + d + e }, '-': function (a, b, c, d, e) { return a - b - c - d - e }, '/': function (a, b, c, d, e) { return a / b / c / d / e }, '*': function (a, b, c, d, e) { return a * b * c * d * e } };
+    result = operators[operatorglobal](one, two, third, fourth, fifth);
+    if (isNaN(result)) {
+        document.getElementById("final").innerHTML = "Please enter a valid value (Hint : Do not enter alphabets and do not leave the first two values empty before pressing the operators.)";
+    }else{
+        document.getElementById("final").innerHTML = "The answer is " + result;
+        console.log("The answer is = " + result)
+    }
+}
+function clearfields() {
+    document.getElementById("one").value = "";
+    document.getElementById("two").value = "";
+    document.getElementById("third").value = "";
+    document.getElementById("fourth").value = "";
+    document.getElementById("fifth").value = "";
+    document.getElementById("final").innerHTML = "";
 }
