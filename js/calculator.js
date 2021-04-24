830150a7052915b33ef84e16fa2d639d01763533
samcodee <samrridhn@gmail.com>
2021-04-24 10:01:40 +0530

    Organized the files and bug fixes

---
+++ js/calculator.js
@@ -0,0 +1,38 @@
+var result;
+
+function addition() {
+    var fno = document.getElementById("fno").value;
+    var sno = document.getElementById("sno").value;
+    if (fno == "" || sno == "") {
+        console.error("no value given");
+        document.getElementById("final").innerHTML = "please enter a value";
+    }
+    else {
+        result = Number(fno) + Number(sno);
+        document.getElementById("final").innerHTML = result;
+        console.log(result);
+    }
+
+}
+
+function subtract() {
+    var fno = document.getElementById("fno").value;
+    var sno = document.getElementById("sno").value;
+    result = Number(fno) - Number(sno)
+    document.getElementById("final").innerHTML = result;
+}
+
+function mult() {
+    var fno = document.getElementById("fno").value;
+    var sno = document.getElementById("sno").value;
+    result = Number(fno) * Number(sno)
+    document.getElementById("final").innerHTML = result;
+}
+
+function divi() {
+    var fno = document.getElementById("fno").value;
+    var sno = document.getElementById("sno").value;
+    result = Number(fno) / Number(sno)
+    document.getElementById("final").innerHTML = result;
+}
+
